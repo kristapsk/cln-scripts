1b2ee1d708fe89ab45876346d8ea76af7e92b9e3
Kristaps Kaupe <kristaps@blogiem.lv>
2022-05-09 08:24:47 +0300

    Simple installation script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [[ "$1" == "-h" ]] | [[ "$1" == "--help" ]]; then
+    echo "Installs CLN scripts."
+    echo "Usage: $(basename "$0")"
+    exit
+fi
+
+PREFIX="/opt/cln-scripts"
+
+if [[ "$(whoami)" == "root" ]]; then
+    sudo=""
+else
+    sudo="sudo"
+fi
+
+cd "$(dirname "$0")" || exit 1
+
+$sudo mkdir -p "$PREFIX"
+
+for script in \
+    channelbalance \
+    feereport \
+    prune-protector \
+    random-traffic-gen \
+    walletbalance
+do
+    echo -n "Installing cln-$script..."
+    $sudo install -p -t "$PREFIX" ./cln-$script.sh
+    $sudo ln -f -s "$PREFIX/cln-$script.sh" /usr/bin/cln-$script
+    echo ""
+done
+
+echo "Done."
