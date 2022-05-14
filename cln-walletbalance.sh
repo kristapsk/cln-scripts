cc79a4da544e4ad2e3b24e52ac2add3b4fa359dd
Kristaps Kaupe <kristaps@blogiem.lv>
2022-05-14 09:46:54 +0300

    Ignore "reserved" outputs
    See https://github.com/ElementsProject/lightning/issues/5249#issuecomment-1126629725
    
    Fixes #1.

--- cln-walletbalance.sh
+++ cln-walletbalance.sh
@@ -16,11 +16,14 @@ outputs="$(lightning-cli listfunds | jq ".outputs")"
 # lightning-cli listfunds | jq ".outputs" | jq -r ".[].status"
 readarray -t output_statuses < <(jq -r ".[].status" <<< "$outputs")
 readarray -t output_values < <(jq -r ".[].value" <<< "$outputs")
+readarray -t output_reserved < <(jq -r ".[].reserved" <<< "$outputs")
 for i in $(seq 0 $(( ${#output_statuses[@]} - 1 )) ); do
-    if [[ "${output_statuses[$i]}" == "confirmed" ]]; then
-        confirmed_balance=$((confirmed_balance + ${output_values[$i]}))
-    elif [[ "${output_statuses[$i]}" == "unconfirmed" ]]; then
-        unconfirmed_balance=$((unconfirmed_balance + ${output_values[$i]}))
+    if [[ "${output_reserved[$i]}" != "true" ]]; then
+        if [[ "${output_statuses[$i]}" == "confirmed" ]]; then
+            confirmed_balance=$((confirmed_balance + ${output_values[$i]}))
+        elif [[ "${output_statuses[$i]}" == "unconfirmed" ]]; then
+            unconfirmed_balance=$((unconfirmed_balance + ${output_values[$i]}))
+        fi
     fi
 done
 total_balance=$((confirmed_balance + unconfirmed_balance))
