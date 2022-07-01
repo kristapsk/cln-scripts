560c0fe94bceecd3fdeaecf2e393783a367eb49f
Kristaps Kaupe <kristaps@blogiem.lv>
2022-07-01 12:50:53 +0300

    Show locked (reserved) balance in cln-walletbalance

--- cln-walletbalance.sh
+++ cln-walletbalance.sh
@@ -12,6 +12,7 @@ fi
 
 confirmed_balance="0"
 unconfirmed_balance="0"
+locked_balance="0"
 outputs="$(lightning-cli listfunds | jq ".outputs")"
 # lightning-cli listfunds | jq ".outputs" | jq -r ".[].status"
 readarray -t output_statuses < <(jq -r ".[].status" <<< "$outputs")
@@ -24,6 +25,8 @@ for i in $(seq 0 $(( ${#output_statuses[@]} - 1 )) ); do
         elif [[ "${output_statuses[$i]}" == "unconfirmed" ]]; then
             unconfirmed_balance=$((unconfirmed_balance + ${output_values[$i]}))
         fi
+    else
+        locked_balance=$((locked_balance + ${output_values[$i]}))
     fi
 done
 total_balance=$((confirmed_balance + unconfirmed_balance))
@@ -31,5 +34,6 @@ total_balance=$((confirmed_balance + unconfirmed_balance))
 echo "{
     \"total_balance\": $total_balance,
     \"confirmed_balance\": $confirmed_balance,
-    \"unconfirmed_balance\": $unconfirmed_balance
+    \"unconfirmed_balance\": $unconfirmed_balance,
+    \"locked_balance\": $locked_balance
 }"
